6b3c1a68517ed5f2565cd90530e588c2a7ee0c56
shanthini2963-IT <shanthiniramanjaneya@gmail.com>
2025-03-03 20:42:15 +0530

    Day2 Lab1 completed

---
+++ problem1.sql
@@ -0,0 +1,3 @@
+INSERT INTO song (Title, Artist) VALUES ('Let it be', 'Beatles');
+INSERT INTO song (Title, Artist) VALUES ('Hotel California', 'Eagles');
+INSERT INTO song (Title, Artist) VALUES ('Kashmir', 'Led Zeppelin');
